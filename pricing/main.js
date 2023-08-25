054e89e9e6f683e502bddfb919cd2260bf97d90a
DeLxCruz <lopezsantiago3503@gmail.com>
2023-08-25 06:48:44 -0500

    optimizacion codigo de las cards

--- pricing/main.js
+++ pricing/main.js
@@ -1,100 +1,68 @@
 const path = "config";
 
 let obtener = async () => {
-    let peticion = await fetch(`${path}.json`);
-    let res = await peticion.json();
-    let seleccion = document.querySelector("#myJSONSection");
-    seleccion.insertAdjacentHTML(
-        "beforeEnd",
+  let peticion = await fetch(`${path}.json`);
+  let res = await peticion.json();
+  let seleccion = document.querySelector("#myJSONSection");
+  seleccion.insertAdjacentHTML(
+    "beforeEnd",
     /*html*/ `
-        <h1 class="display-6 fw-normal text-body-emphasis mb-4">${res.section.titulo
+        <h1 class="display-6 fw-normal text-body-emphasis mb-4">${
+          res.section.titulo
         }</h1>
         <div class="fs-5 text-body-secondary">
             ${res.section.parrafo
-            .map(
+              .map(
                 (element) => /*html*/ `
                     <p>${element.icono}
                     ${element.texto}</p>
                 `
-            )
-            .join("")}
+              )
+              .join("")}
         </div>
     `
-    );
-    let cardSelection = document.querySelector("#cardJSONSection");
-    cardSelection.insertAdjacentHTML(
-        "beforeend",
+  );
+  let cardSelection = document.querySelector("#cardJSONSection");
+  cardSelection.insertAdjacentHTML("beforeend",
     /*html*/ `
-        <div class="col">
-            <div class="card mb-4 rounded-3 shadow-sm">
-                <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal">
-                        ${res.cardSection[0].tipo}
-                    </h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">${res.cardSection[0].precio}<small class="text-body-secondary fw-light">/mo</small></h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                        ${res.cardSection[0].beneficios.map(
-                            (element) => /*html*/ `
-                                <li>${element}</li>
-                            `
-                            ).join("")}
-                    </ul>
-                    <button type="button" class="w-100 btn btn-lg btn-outline-primary">Next</button>
-                </div>
-            </div>
-        </div>
+    ${res.cardSection.map((value) => 
+        /*html*/ `
         <div class="col">
         <div class="card mb-4 rounded-3 shadow-sm">
             <div class="card-header py-3">
                 <h4 class="my-0 fw-normal">
-                    ${res.cardSection[1].tipo}
+                    ${value.tipo}
                 </h4>
             </div>
             <div class="card-body">
-                <h1 class="card-title pricing-card-title">${res.cardSection[1].precio}<small class="text-body-secondary fw-light">/mo</small></h1>
+                <h1 class="card-title pricing-card-title">${value.precio
+                }<small class="text-body-secondary fw-light">/mo</small></h1>
                 <ul class="list-unstyled mt-3 mb-4">
-                    ${res.cardSection[1].beneficios.map(
+                    ${value.beneficios.map(
                         (element) => /*html*/ `
                             <li>${element}</li>
                         `
-                        ).join("")}
+                      )
+                      .join("")}
                 </ul>
                 <button type="button" class="w-100 btn btn-lg btn-outline-primary">Next</button>
             </div>
         </div>
-    </div>
-            <div class="col">
-            <div class="card mb-4 rounded-3 shadow-sm border-primary">
-                <div class="card-header py-3 text-bg-primary border-primary">
-                    <h4 class="my-0 fw-normal">
-                        ${res.cardSection[2].tipo}
-                    </h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">${res.cardSection[2].precio}<small class="text-body-secondary fw-light">/mo</small></h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                        ${res.cardSection[2].beneficios.map(
-                            (element) => /*html*/ `
-                                <li>${element}</li>
-                            `
-                            ).join("")}
-                    </ul>
-                    <button type="button" class="w-100 btn btn-lg btn-outline-primary">Next</button>
-                </div>
-            </div>
-        </div>
+    </div>`).join("")}
         `
-    );
+  )
 
-    let textSectionTable = document.querySelector('#textSectionTable')
-    textSectionTable.insertAdjacentHTML('beforeend', 
-        /*html*/`
+  let textSectionTable = document.querySelector("#textSectionTable");
+  textSectionTable.insertAdjacentHTML(
+    "beforeend",
+    /*html*/ `
             <p class="link-secondary">
                 ${res.textSectionTable}
             </p>
-    `)
+    `
+  );
 };
 
 obtener();
+
+
