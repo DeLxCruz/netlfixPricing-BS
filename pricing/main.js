dde2ba962e02833339bfad4bab273972195e5b39
DeLxCruz <lopezsantiago3503@gmail.com>
2023-08-25 06:59:13 -0500

    quitandole el rounded a las cards

--- pricing/main.js
+++ pricing/main.js
@@ -28,7 +28,7 @@ let obtener = async () => {
     ${res.cardSection.map((value) => 
         /*html*/ `
         <div class="col">
-        <div class="card mb-4 rounded-3 shadow-sm">
+        <div class="card mb-4 rounded-0 shadow-sm">
             <div class="card-header py-3">
                 <h4 class="my-0 fw-normal">
                     ${value.tipo}
